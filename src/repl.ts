cf0d895f103589a8bbcc3a0e1a712cf56abf181b
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2026-08-27 19:02:52 +0300

    1.4 repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,28 @@
+import * as readline from "node:readline";
+
+const inputInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+});
+
 export function cleanInput(input: string): string[] {
     return input.split(' ').map((item) => item.trim().toLowerCase()).filter((item) => item !== '');
+}
+
+export function startREPL() {
+    inputInterface.prompt();
+
+    inputInterface.on("line", (line: string) => {
+        const words = cleanInput(line);
+        if (words.length === 0) {
+            inputInterface.prompt();
+            return;
+        }
+
+        // TODO: dispatch on words[0] as the command name
+        console.log(`Your command was: ${words[0]}`);
+
+        inputInterface.prompt();
+    });
 }
