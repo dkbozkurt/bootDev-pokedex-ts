8de3d9ca37847afa7680f4a48eaea2979e4b0a7c
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2026-08-31 16:10:51 +0300

    1.5 help and exit

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,7 @@
 import * as readline from "node:readline";
 
+import { getCommands } from "./command.js";
+
 const inputInterface = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -10,7 +12,18 @@ export function cleanInput(input: string): string[] {
     return input.split(' ').map((item) => item.trim().toLowerCase()).filter((item) => item !== '');
 }
 
+export function logWelcomeMessage() {
+    console.log(`Welcome to the Pokedex!
+Usage:\n\n`);
+
+    for (const command of Object.values(getCommands())) {
+        console.log(`  ${command.name}: ${command.description}`);
+    }
+
+}
+
 export function startREPL() {
+    logWelcomeMessage();
     inputInterface.prompt();
 
     inputInterface.on("line", (line: string) => {
@@ -21,7 +34,14 @@ export function startREPL() {
         }
 
         // TODO: dispatch on words[0] as the command name
-        console.log(`Your command was: ${words[0]}`);
+        // console.log(`Your command was: ${words[0]}`);
+
+        if (words[0] in getCommands()) {
+            const command = getCommands()[words[0]];
+            command.callback(getCommands());
+        } else {
+            console.log(`Unknown command: ${words[0]}`);
+        }
 
         inputInterface.prompt();
     });
